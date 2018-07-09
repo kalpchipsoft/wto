2195510111716e6af81f96c3ae905bc68f649335
Ashvini Mishra <ashvini.chipsoft@gmail.com>
2018-07-09 15:49:00 +0530

    Global Error changes

---
+++ BusinessObjects/GlobalErrorModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessObjects
+{
+    public class GlobalErrorModel
+    {
+       
+        public string Detail { get; set; }
+        public string Subject { get; set; }
+        public string RefrenceNo { get; set; }
+        public string SentBy { get; set; }
+    }
+
+    public class  GlobalResult : CommonResponseModel
+    {
+        public string Status { get; set; }
+    }
+}
--- WTO/Global.asax.cs
+++ WTO/Global.asax.cs
@@ -1,7 +1,10 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WTO.Controllers;
+using WTO.Controllers.WTO;
 
 namespace WTO
 {
@@ -16,6 +19,44 @@ namespace WTO
             App_Start.FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             App_Start.BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error()
+        {
+
+            GlobalErrorController objGlobalError = new GlobalErrorController();
+            LoginController objloginController = new LoginController();
+            var excepMsg = "";
+            var source = "";
+
+
+            Exception exception = Server.GetLastError();
+            excepMsg = exception.Message;
+            source = exception.Source;
+            objGlobalError.Error(excepMsg, source);
+            Server.ClearError();
+
+            //HttpContext.Current.Response.Redirect("~/Views/Shared/Error");
+            //Debug.WriteLine("");
+            //Response.Redirect("~/Views/_ViewStart.cshtml");
+            Application_GlobalError();
+
+        }
+
+        protected void Application_GlobalError()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+
+            // RouteConfig.RegisterRoutes1(RouteTable.Routes);
+            App_Start.FilterConfig.RegisterGlobalFilters1(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes1(RouteTable.Routes);
+            Response.RedirectToRoute("404-PageNotFound");
+        }
+
+        protected void Application_End()
+        {
+
         }
     }
 }
